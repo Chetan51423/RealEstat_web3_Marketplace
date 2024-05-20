04a2e10836d52e3c57e963fe7d3ad81ff2d3406e
Chetan51423 <chetanshinde51423@gamil.com>
2024-05-20 09:49:22 +0530

    real estate marketplace code

---
+++ PageComponents/AboutPage/Action.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Action = () => {
+  return (
+    <div class="rn-callto-action rn-section-gapTop">
+      <div class="container-fluid about-fluidimg-cta">
+        <div class="row">
+          <div class="col-lg-12">
+            <div
+              class="bg_image--6 bg_image bg-image-border"
+              data-black-overlay="7"
+            >
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="call-to-action-wrapper">
+                    <h3
+                      data-sal="slide-up"
+                      data-sal-duration="800"
+                      data-sal-delay="150"
+                    >
+                      Discover listed Properties <br /> sell and buy them
+                    </h3>
+                    <p
+                      data-sal="slide-up"
+                      data-sal-duration="800"
+                      data-sal-delay="150"
+                    >
+                      The  is blockchain one-trick pony that climbed the recent
+                      years. The growth of DAPPs is tremendous, the RealEstate sectore need some secure and advance platform to buy and sell properties.
+                    </p>
+                    <div
+                      class="callto-action-btn-wrapper"
+                      data-sal="slide-up"
+                      data-sal-duration="800"
+                      data-sal-delay="150"
+                    >
+                      <a href="/create" class="btn btn-primary btn-large">
+                        List Property
+                      </a>
+                      <a href="/" class="btn btn-primary btn-large">
+                        Explore Property
+                      </a>
+
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Action;
---
+++ PageComponents/Components/Service.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+
+const Service = () => {
+  return (
+    <div class="rn-service-area rn-section-gapTop">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 mb--50">
+            <h3
+              class="title"
+              data-sal-delay="150"
+              data-sal=""
+              data-sal-duration="800"
+            >
+              List and Sell your Properties
+            </h3>
+          </div>
+        </div>
+        <div class="row g-5">
+          <div class="col-xxl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+            <div
+              data-sal=""
+              data-sal-delay="150"
+              data-sal-duration="800"
+              class="rn-service-one color-shape-7"
+            >
+              <div class="inner">
+                <div class="icon">
+                  <img src="/icons/shape-7.png" alt="Shape" />
+                </div>
+                <div class="subtitle">Step-01</div>
+                <div class="content">
+                  <h4 class="title">
+                    <a href="#">Set up your wallet</a>
+                  </h4>
+                  <p class="description">
+                   Connect your wallet by simply clicking connect option at the nav bar and connect. 
+                   You can connect different wallets. 
+                  </p>
+                  <a class="read-more-button" href="#">
+                    <i class="feather-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+              <a class="over-link" href="#"></a>
+            </div>
+          </div>
+
+          <div class="col-xxl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+            <div
+              data-sal="slide-up"
+              data-sal-delay="200"
+              data-sal-duration="800"
+              class="rn-service-one color-shape-1"
+            >
+              <div class="inner">
+                <div class="icon">
+                  <img src="/icons/shape-1.png" alt="Shape" />
+                </div>
+                <div class="subtitle">Step-02</div>
+                <div class="content">
+                  <h4 class="title">
+                    <a href="#">Create your collection</a>
+                  </h4>
+                  <p class="description">
+                    You can create collection of properties of your choise and explore the realism of 
+                    web3 marketplace.
+                  </p>
+                  <a class="read-more-button" href="#">
+                    <i class="feather-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+              <a class="over-link" href="#"></a>
+            </div>
+          </div>
+
+          <div class="col-xxl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+            <div
+              data-sal="slide-up"
+              data-sal-delay="250"
+              data-sal-duration="800"
+              class="rn-service-one color-shape-5"
+            >
+              <div class="inner">
+                <div class="icon">
+                  <img src="/icons/shape-5.png" alt="Shape" />
+                </div>
+                <div class="subtitle">Step-03</div>
+                <div class="content">
+                  <h4 class="title">
+                    <a href="#">Add your Properties</a>
+                  </h4>
+                  <p class="description">
+                    Once your wallet is connected you can add properties to the the web3 marketplace.
+
+                  </p>
+                  <a class="read-more-button" href="#">
+                    <i class="feather-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+              <a class="over-link" href="#"></a>
+            </div>
+          </div>
+
+          <div class="col-xxl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+            <div
+              data-sal="slide-up"
+              data-sal-delay="300"
+              data-sal-duration="800"
+              class="rn-service-one color-shape-6"
+            >
+              <div class="inner">
+                <div class="icon">
+                  <img src="/icons/shape-6.png" alt="Shape" />
+                </div>
+                <div class="subtitle">Step-04</div>
+                <div class="content">
+                  <h4 class="title">
+                    <a href="#">Buy and Sell your Properties</a>
+                  </h4>
+                  <p class="description">
+                    Now you can Buy and sell properties that you like.
+                  </p>
+                  <a class="read-more-button" href="#">
+                    <i class="feather-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+              <a class="over-link" href="#"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
